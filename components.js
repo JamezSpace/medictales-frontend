b171e6d1a25ed949c4faa1183c9386c0885413e4
Jamez <130938946+JamezSpace@users.noreply.github.com>
2023-12-05 16:38:53 +0100

    First Commit

---
+++ components.js
@@ -0,0 +1,80 @@
+function openNotifBox() {
+    document.getElementById("open-notifications").classList.add("active")
+    document.body.classList.add("modal-open")
+}
+function openTaskDialog() {
+    document.getElementById("task-dialog").classList.add("active")
+    document.body.classList.add("modal-open")
+}
+
+function openFilterDialog() {
+    document.getElementById("filter-dialog").classList.toggle("active")
+}
+/**
+ * Take note, this is equivalent to pressing the exit button on the dialog or tab. Hence, the element argument has to specifically point to the exit button (either by id or class), also the tab or dialog to close has to be specified as a data-close attribute in the HTML code of the exit button.
+ * 
+ * @param {HTMLElement} element 
+ */
+function triggerExitButton(element) {
+    document.getElementById(element.dataset.close).classList.remove("active")
+}
+
+let count = 0, numUsed = []
+function generateTaskNumber() {
+    let max_number_of_tasks = 11
+    if (count === max_number_of_tasks) {
+        count %= 11
+        numUsed.splice(0, numUsed.length)
+    }
+    if (!numUsed.includes(count)) numUsed.push(count)
+
+    count++
+
+    return count
+}
+
+function addNewTask() {
+    if (handleNewTaskInput().valid !== 'yes') return
+    const text = handleNewTaskInput().task
+    const num = generateTaskNumber()
+
+    // create the elements and set the attributes
+    let section = document.createElement('section')
+    section.setAttribute('class', 'task')
+    let checkbox_within = document.createElement('input')
+    checkbox_within.setAttribute('type', 'checkbox')
+    checkbox_within.setAttribute('id', `task-${num}`)
+    checkbox_within.addEventListener("change", e => {
+        section.classList.toggle("task-completed")
+    })
+    let label = document.createElement('label')
+    label.setAttribute('for', `task-${num}`)
+    label.innerText = text
+
+    // add the checkbox and text to the task 
+    section.appendChild(checkbox_within)
+    section.appendChild(label)
+
+    // add the task to the TO-DO task section
+    document.getElementById("tasks-container").appendChild(section)
+
+    // close the dialog box
+    // triggerExitButton(document.getElementsByClassName("exit")[0])
+}
+
+function handleNewTaskInput() {
+    const text_field = document.getElementById("write-task")
+    const date = document.getElementById("select_date")
+
+    if (text_field.value.trim().length === 0) text_field.classList.add("invalid")
+    else if (text_field.value.trim().length !== 0) text_field.classList.remove("invalid")
+
+    console.log(date.value)
+
+    const text = text_field.value[0].toUpperCase() + text_field.value.substring(1, text_field.value.length)
+
+
+    return { valid: 'yes', task: text }
+}
+
+7560574761976
---
+++ login_signup.js
@@ -0,0 +1,192 @@
+
+
+function switch_to_login() {
+    document.getElementById("sign-up-page").classList.remove("page-active");
+    document.getElementById("sign-up-tab").classList.remove("btn-active");
+
+    document.getElementById("login-page").classList.add("page-active");
+    document.getElementById("login-tab").classList.add("btn-active");
+}
+
+function switch_to_signup() {
+    document.getElementById("sign-up-page").classList.add("page-active");
+    document.getElementById("sign-up-tab").classList.add("btn-active");
+
+    document.getElementById("login-page").classList.remove("page-active");
+    document.getElementById("login-tab").classList.remove("btn-active");
+}
+
+// slideshow functionality
+const slides = document.querySelectorAll("section.slide");
+let i = 0;
+
+setInterval(() => {
+    console.log(i);
+    slides[i].classList.remove("active");
+    if (i == slides.length - 1) i = -1;
+    slides[i + 1].classList.add("active");
+    i++;
+}, 2000)
+
+// validate inputs
+function validate_name(name) {
+    let obj = { valid: true, msg: null, field: null }
+    const username = name.trim().toLowerCase()
+
+    // checks for empty string
+    if (username.length === 0) obj = { valid: false, msg: "Fill in your name", field: "error-name" }
+
+    // checks for any digit
+    else if (/\d/.test(username)) obj = { valid: false, msg: "Remove the digit in the name", field: "error-name" }
+
+    // checks for any special character
+    else if (!/^[a-zA-Z]+$/.test(username)) obj = { valid: false, msg: "Remove the symbol in the name", field: "error-name" }
+
+    return obj
+}
+
+function validate_email(email) {
+    let obj = { valid: true, msg: null, field: null }
+    const useremail = email.trim().toLowerCase()
+    const email_array = useremail.split('@')
+
+    // ensures first part of email address is more than 2 characters
+    if (email_array[0].length < 2) obj = { valid: false, msg: "Email is not valid", field: "error-email" };
+
+    // ensures only gmail is accepted (@gmail.com)
+    else if (email_array[1] !== "gmail.com") obj = { valid: false, msg: "Use a gmail account '@gmail.com'", field: "error-email" }
+
+    return obj
+}
+
+function validate_member_or_writer() {
+    let obj = { valid: true, msg: null, field: null }, checked_input;
+    const inputs = document.querySelectorAll("div.memberField section.selectOne span.single-field input[type=\"radio\"]")
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) checked_input = inputs[i].id
+    }
+
+    if (typeof checked_input === 'undefined') obj = {
+        valid: false,
+        msg: "Select One!",
+        field: "error-member"
+    }
+
+    console.log(obj);
+    return obj;
+}
+
+// TODO
+/**
+ * Function validates password
+ * @param {string} password 
+ */
+function validate_password(password) {
+    let password_valid = true
+    const userpassword = password.trim()
+
+    // ensures password has at least 5 characters
+    if (userpassword.length < 5) {
+        password_valid = false
+        const icon = document.getElementById("check-length").children[0];
+
+        if (icon.classList.contains("valid")) icon.classList.replace("valid", "invalid")
+        else if (!icon.classList.contains("invalid")) icon.classList.add("invalid")
+    } else document.getElementById("check-length").children[0].classList.replace("invalid", "valid")
+
+    // ensures password combines letters and numbers
+    if (!(/\d/.test(userpassword) && /[a-zA-Z]/.test(userpassword))) {
+        password_valid = false
+        const icon = document.getElementById("check-alnumeric").children[0];
+
+        if (icon.classList.contains("valid")) icon.classList.replace("valid", "invalid")
+        else if (!icon.classList.contains("invalid")) icon.classList.add("invalid")
+    } else document.getElementById("check-alnumeric").children[0].classList.replace("invalid", "valid")
+
+    return password_valid;
+}
+
+
+/**
+ * Function writes error messages in an error bar but both the message and the 'id' of the field to write the message is passed as arguments.
+ * @param {string} message 
+ * @param {string} field_to_write_error_message 
+ */
+function write_error_message(message, field_to_write_error_message) {
+    const error_bars = document.querySelectorAll("div.error-bar");
+
+    for (let i = 0; i < error_bars.length; i++) {
+        const each_error_bar = error_bars[i];
+        console.log(field_to_write_error_message, each_error_bar.id);
+
+        if (field_to_write_error_message === each_error_bar.id) {
+            // the childNode at index 1 is the span element displaying the message
+            each_error_bar.children[1].textContent = message;
+            each_error_bar.classList.add("make-visible")
+        }
+    }
+}
+
+
+/**
+ * To default inputs is simply to remove the error bars, hence, code finds all error bars that are visible and makes invisble.
+ */
+function remove_error_bars() {
+    const error_bars = document.querySelectorAll("div.error-bar");
+
+    for (let i = 0; i < error_bars.length; i++) {
+        const each_error_bar = error_bars[i];
+
+        each_error_bar.classList.remove("make-visible")
+    }
+}
+
+function validate_all_inputs(form) {
+    let all_valid = true;
+
+    const email_validation_result = validate_email(document.getElementById("email-signup").value || document.getElementById("email-login").value);
+
+    let input_array = [email_validation_result]
+
+    if (form === 'sign-up') {
+        const name_validation_result = validate_name(document.getElementById("name").value);
+        const member_field_result = validate_member_or_writer()
+
+        input_array.push(name_validation_result, member_field_result)
+
+        if (!validate_password(document.getElementById("password-signup").value)) all_valid = false
+    }
+
+    remove_error_bars()
+    console.log(input_array);
+
+    for (let i = 0; i < input_array.length; i++) {
+        const each_input = input_array[i];
+
+        if (!each_input.valid) {
+            all_valid = false;
+
+            write_error_message(each_input.msg, each_input.field);
+            break;
+        }
+    }
+
+    return all_valid;
+}
+
+document.getElementById("sign-up-form").addEventListener("submit", e => {
+    console.log(e);
+    if (!validate_all_inputs("sign-up")) e.preventDefault()
+})
+
+
+// document.getElementById("submit-btn").addEventListener("click", e => {
+//     console.log(e);
+//     if (!validate_all_inputs()) e.preventDefault()
+// })
+
+document.getElementById("login-form").addEventListener("submit", e => {
+    console.log(e);
+    if (!validate_all_inputs("login")) e.preventDefault()
+})
